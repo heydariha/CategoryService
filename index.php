816a4b7e2ab2f9c1c545af1cf63a4f76d36c1259
HAdi <heydariham@farhoonet.ir>
2017-09-30 22:47:22 +0330

    Change in index.php

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ use AboutYou\Service\validateProduct;
 use AboutYou\Service\ProductServiceInterface;
 use AboutYou\Service\UnorderedProductService;
 
-
 $obj    = \AboutYou\Service\ProductFactory::create();
 $result = $obj->getProductsForCategory('Clothes');
 // echo "<pre>";print_r($result);exit;
