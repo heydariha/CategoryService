919b389f9d5e7da2df8c9bd2de484dea238d0a2c
HAdi <heydariham@farhoonet.ir>
2017-10-01 01:02:12 +0330

    Add Description to class

--- src/AboutYou/Service/UnorderedProductService.php
+++ src/AboutYou/Service/UnorderedProductService.php
@@ -47,7 +47,12 @@ class UnorderedProductService implements ProductServiceInterface
         $productResults = $this->makeSortProducts($productResults,"description");
         return $productResults;
     }
-
+    /**
+    *   @param  array $productsArr
+    *   @param string $key
+    *
+    *   @return products[]
+    **/
     public function makeSortProducts(array $productsArr,$key)
     {
         usort($productsArr, function($productA, $productB) use ($key) {
